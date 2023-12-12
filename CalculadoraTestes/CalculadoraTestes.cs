985dcb529f1cee77f088fc988e1244de29c479c6
thiagolluiz <delimaluiz.thiago@gmail.com>
2023-12-12 11:43:03 -0300

    first deploy

---
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -0,0 +1,57 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+    private CalculadoraImp _calc;
+
+    public CalculadoraTestes()
+    {
+        _calc = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void SomarNUmTest() // deve somar 10 com 5 e retornar 15
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Act
+        int resultado = _calc.Somar(num1, num2);
+
+        // Assert
+        Assert.Equal(15, resultado);
+
+    }
+
+    [Fact]
+    public void SomarNUmTest2() // deve somar 10 com 10 e retornar 20
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 10;
+        
+        // Act
+        int resultado = _calc.Somar(num1, num2);
+
+        // Assert
+        Assert.Equal(20, resultado);
+    }
+
+
+    [Theory] // testando varios numeros no mesmo metodo
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    public void SomarNUmTest3( int numero) // deve verificar se é PAR, deve setar (int numero)
+    {
+        // Act
+        bool resultado = _calc.EhPar(numero);
+        // Assert
+        Assert.True(resultado);
+    }
+}
